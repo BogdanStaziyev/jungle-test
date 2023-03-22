be78939adadf2796d82f7f5f541a278d00dc41d8
staziyev <staziyev@gmail.com>
2023-03-22 15:32:16 +0200

    fix: naming in request

--- internal/controller/http/requests/auth_request.go
+++ internal/controller/http/requests/auth_request.go
@@ -7,7 +7,7 @@ import (
 
 type RequestUser struct {
 	Password string `json:"password" validate:"required,gte=8"`
-	Name     string `json:"name" validate:"required,gte=3"`
+	Name     string `json:"username" validate:"required,gte=3"`
 }
 
 func (r RequestUser) RegisterToUser() entity.User {
